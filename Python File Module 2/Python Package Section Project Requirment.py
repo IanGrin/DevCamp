d14b51ccd4ed5f7b377432f8d86a20bf51116005
IanGrin <iangrin.coding@gmail.com>
2023-02-17 11:03:07 -0700

    ZTM and Devcamp Study

---
+++ Python File Module 2/Python Package Section Project Requirment.py
@@ -0,0 +1,14 @@
+# 'https://www.dailysmarty.com/posts/how-to-implement-fizzbuzz-in-python'
+# "How to Implement FizzBuzz in Python"
+# "Installing Anaconda Python Data Science Platform"
+# "Python Script for Pulling in the same Column from an Array of Arrays"
+
+# Libraries:
+
+# - requests
+# - inflection
+# - beautifulsoup
+
+# pip install requests
+# pip install inflection
+# pip install beautifulsoup4
