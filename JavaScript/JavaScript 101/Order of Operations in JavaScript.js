7aa84f95f3f0a7033998493fae4192bed7fb4e8f
Ian Merritt <iangrin.coding@gmail.com>
2023-03-12 11:19:20 -0600

    classwork

---
+++ JavaScript/JavaScript 101/Order of Operations in JavaScript.js
@@ -0,0 +1,12 @@
+/*  PEMDAS -> PEDMAS
+    Please Excuse My Dear Aunt Sally - PEMDAS
+    Paranthesis
+    Exponents
+    Multiplication
+    Division
+    Addition
+    Subtraction
+*/
+
+var num = 5 + (5 * 10) / 6**2 - 1;
+console.log(num);
